bee07759c68a9eae487a9333b39ae541a7a75927
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-12 13:15:03 -0500

    Fetching appropriate products and data

--- javascripts/acme.js
+++ javascripts/acme.js
@@ -4,6 +4,7 @@ var Acme = (function(acme) {
   function fetchCategories(selectedId) {
     return new Promise(function(resolve, reject) {
       $.ajax("/data/categories.json")
+        //NOTE(adam): resolve with the single category matching the selectedId
         .done(data => resolve(data.categories.filter(e => e.id === selectedId)[0]))
         .fail((x,s,e) => reject(e));
     });
@@ -12,6 +13,7 @@ var Acme = (function(acme) {
   function fetchTypes(categoryId) {
     return new Promise(function(resolve, reject) {
       $.ajax("/data/types.json")
+        //NOTE(adam): resolve with the list of types that match the categoryId
         .done(data => resolve(data.types.filter(e => e.category === categoryId)))
         .fail((x,s,e) => reject(e));
     });
@@ -21,6 +23,7 @@ var Acme = (function(acme) {
     return new Promise(function(resolve, reject) {
       $.ajax("/data/products.json")
         .done(data => {
+          //NOTE(adam): resolve with the list of products matching any type in typeArray
           let typeIds = typeArray.map(t => t.id);
           resolve(data.products.filter(e => typeIds.indexOf(e[Object.keys(e)[0]].type) > -1));
         })
@@ -33,18 +36,15 @@ var Acme = (function(acme) {
     var types = [];
     var products = [];
 
-    return fetchCategories(selectedId).then(function(data) {
-      console.log("cat data", data);
+    fetchCategories(selectedId).then(function(data) {
       category = data;
       return fetchTypes(category.id);
     }).then(function(data) {
-      console.log("type data", data);
       types = data;
       return fetchProducts(types);
     }).then(function(data) {
-      console.log("prod data", data);
       products = data;
-      return callback({ category, types, products });
+      callback({ category, types, products });
     });
   };
 
